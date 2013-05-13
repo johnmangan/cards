9008a90e187e833c46ade41cb98e73cb4b427eed
John Mangan <jmangan@eng.ucsd.edu>
2013-05-13 14:38:31 -0700

    * Added the randomness desire to the SearchLogicHandler API.

--- modules/SearchLogicHandler.h
+++ modules/SearchLogicHandler.h
@@ -12,22 +12,24 @@ class SearchLogicHandler
 public:
 
     /* Funtion: getFilepath
-     * Input: Map of Metadata names (e.g, hash-tags) and an integer weight for each.
+     * Input: metadataWeights- Map of Metadata names (e.g, hash-tags) and an integer
+     *                         weight for each.
+     *        percent- Value from 0.0 to 1.0 representing how close to optimal is desired.
+     *                 0.0 implies the desire for any asset that has some correlation to
+     *                 the metadata given. 1.0 implies the desire for the highest ranked
+     *                 asset calculated from the given metadata. 
      * Output: Ordered filepaths accessible via the local filesystem to the chosen asset.
      *         Filepaths will be ordered from lowest to highest level of detail.
      *
      * The weights are meant to be proportional to their sum,
-     * thus all weight are relative to the others provided.
+     * thus all weights are relative to the others provided.
      */
 
     // TODO: add in support for returning based on top percentile of hits,
     //       or based on a minimum total weight threshold, or both
 
-    // TODO: add in support for LOD requirements... discuss with Andrew on Monday
-    //       as he has a differing idea of how it may be used than we do
-
     virtual std::list< std::string >
-    getFilepaths( std::map< std::string, int > metadataWeights ) = 0;
+    getFilepaths( std::map< std::string, int > metadataWeights, double percent = 1.0 ) = 0;
 }
 
 #endif /*SEARCH_LOGIC_HANDLER_H_*/
