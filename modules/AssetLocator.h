12af789e998a55c43e2a0751cf3bf90aeae60cab
John Mangan <jmangan@eng.ucsd.edu>
2013-05-20 13:25:13 -0700

    * Updated AssetLocator - previously updated external interfaces and mapping module to correlated numerous LODs with a single asset, but forgot to update AssetLocator.

--- modules/AssetLocator.h
+++ modules/AssetLocator.h
@@ -3,6 +3,7 @@ namespace cards {
 #ifndef ASSET_LOCATOR_H_
 #define ASSET_LOCATOR_H_
 
+#include <vector>
 #include <string>
 
 #include "AssetTag.h"
@@ -11,17 +12,26 @@ class AssetLocator
 {
 public:
 
-    virtual std::string
+    // Query Functionality
+    virtual std::vector< std::string >
     getFilepath(AssetTag assetTag) = 0;
 
+    // Asset Management
     virtual void
-    addAsset( AssetTag assetTag, std::string location ) = 0;
+    addAsset( AssetTag assetTag ) = 0;
 
     virtual void
     removeAsset( AssetTag assetTag ) = 0;
 
+    // Location Management
     virtual void
-    updateLocation( AssetTag assetTag, std::string location ) = 0;
+    addLevelOfDetailLocation( AssetTag assetTag, int lod, std::string location );
+
+    virtual void
+    removeLevelOfDetailLocation( AssetTag assetTag, int lod );
+
+    virtual void
+    updateLevelOfDetailLocation( AssetTag assetTag, int lod, std::string location );
 }
 
 #endif /*ASSET_LOCATOR_H_*/
