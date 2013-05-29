6b04a21034376406bdb6ff255366eb6891749513
Samuel <samuel@samuel-Rev-1-0.(none)>
2013-05-29 11:56:23 -0700

    Add test/main.cpp for template and make sure CMake build

---
+++ test/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "AssetLocatorDatabase.h"
+#include "AssetLocatorDatabaseImpl.h"
+#include "AssetLocatorSQLLiteDatabase.h"
+
+using namespace std;
+
+int main (int argc, char *argv[])
+{
+   
+   cout << "hello world" << endl;
+   return(0);
+}
+
