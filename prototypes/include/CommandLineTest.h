c9a6459d222e93c5549913b84a34306186cd8ce3
edward <ed@ubuntu.(none)>
2013-06-03 14:23:34 -0700

    Command Line Input Test

---
+++ prototypes/include/CommandLineTest.h
@@ -0,0 +1,17 @@
+#ifndef COMMANDLINETEST_H_
+#define COMMANDLINETEST_H_
+
+#include <string>
+#include <vector>
+
+namespace cards {
+
+class CommandLineTest
+{
+public:
+
+};
+
+}
+
+#endif /*COMMANDLINETEST_H_*/
