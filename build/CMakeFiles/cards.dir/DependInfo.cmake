0049277aafefec98b6a0bd1b9fde19fb21170b7c
edward <ed@ubuntu.(none)>
2013-06-04 16:18:00 -0700

    testing command line

---
+++ build/CMakeFiles/cards.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ed/opt/cards/src/AccessManagerImpl.cpp" "/home/ed/opt/cards/build/CMakeFiles/cards.dir/src/AccessManagerImpl.cpp.o"
+  "/home/ed/opt/cards/src/AssetLocatorDatabaseImpl.cpp" "/home/ed/opt/cards/build/CMakeFiles/cards.dir/src/AssetLocatorDatabaseImpl.cpp.o"
+  "/home/ed/opt/cards/src/AssetLocatorMemoryImpl.cpp" "/home/ed/opt/cards/build/CMakeFiles/cards.dir/src/AssetLocatorMemoryImpl.cpp.o"
+  "/home/ed/opt/cards/src/AssetLocatorSQLLiteDatabase.cpp" "/home/ed/opt/cards/build/CMakeFiles/cards.dir/src/AssetLocatorSQLLiteDatabase.cpp.o"
+  "/home/ed/opt/cards/src/AssetMetadataMappingMemoryImpl.cpp" "/home/ed/opt/cards/build/CMakeFiles/cards.dir/src/AssetMetadataMappingMemoryImpl.cpp.o"
+  "/home/ed/opt/cards/src/AssetTagImpl.cpp" "/home/ed/opt/cards/build/CMakeFiles/cards.dir/src/AssetTagImpl.cpp.o"
+  "/home/ed/opt/cards/src/DescriptionInterfaceImpl.cpp" "/home/ed/opt/cards/build/CMakeFiles/cards.dir/src/DescriptionInterfaceImpl.cpp.o"
+  "/home/ed/opt/cards/src/MetadataTagImpl.cpp" "/home/ed/opt/cards/build/CMakeFiles/cards.dir/src/MetadataTagImpl.cpp.o"
+  "/home/ed/opt/cards/src/QueryInterfaceImpl.cpp" "/home/ed/opt/cards/build/CMakeFiles/cards.dir/src/QueryInterfaceImpl.cpp.o"
+  "/home/ed/opt/cards/src/RankedSearchImpl.cpp" "/home/ed/opt/cards/build/CMakeFiles/cards.dir/src/RankedSearchImpl.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
---
+++ build/CMakeFiles/sqlite3-shared.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/sqlite3-shared.dir/sqlite/src/sqlite3.c.o"
+  "libsqlite3.pdb"
+  "libsqlite3.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/sqlite3-shared.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
